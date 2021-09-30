ab816348f476e17067150d3a3f37f8285a751d3a
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-09-30 18:19:04 +0200

    Added functionality to the city api

--- 5._AJAX/js/kea_IT_degrees.js
+++ 5._AJAX/js/kea_IT_degrees.js
@@ -6,15 +6,18 @@ $(document).ready(function () {
 
 function appendJsonData() {
     $.getJSON("http://localhost/web_app_xampp_tasks/5._ajax/data/info.json", function (data) {
-        console.log("data elm", data.kea_it_degrees);
-
-
         for (let i = 0; i < data.kea_it_degrees.length; i++) {
             $("#main").append(($("<article></article>").attr("id", data.kea_it_degrees[i].id)))
             $("#" + data.kea_it_degrees[i].id).append("<section></section>");
             $("#" + data.kea_it_degrees[i].id + "> section").append("<p></p>");
             $("#" + data.kea_it_degrees[i].id + "> section > p").append(data.kea_it_degrees[i].description);
+            console.log("semesters length", data.kea_it_degrees[i].semesters.length)
+            for (let j = 0; j < data.kea_it_degrees[i].semesters.length; j++) {
 
+                $("#" + data.kea_it_degrees[j].id).append("<section class=\"semester\"></section>");
+
+                // $(".semester").append("<header></header>, <article></article>");
+            }
             // .append("<header></header>");
 
         }
--- 5._AJAX/js/townInformation.js
+++ 5._AJAX/js/townInformation.js
@@ -3,17 +3,31 @@ $(document).ready(function () {
 });
 
 function getWeatherAPI() {
-    console.log("working)==")
     $("#townBtn").click(function () {
-        let city_name = $("#cityInput").val();
-        console.log(city_name)
-        let url = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=7b465694a9d40b359790061668d4c801";
-        console.log(url)
-        $.get(url, function (data) {
-            console.log(data)
-        })
+        let cityName = $("#cityInput").val();
+        let weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=7b465694a9d40b359790061668d4c801";
+        $.get(weatherUrl, function (data) {
+            $(".weatherMapSection").remove() //Removing the class section before appending a new one.
 
-    })
+            $("main")
+                .append($("<section></section>").attr("class", "weatherMapSection")
+                    .append($("<div></div>")
+                        .append($("<h1></h1>").text(data.name + ", " + data.sys.country))
+                        .append($("<b></b>").text(data.weather[0].main))
+                        .append($("<p></p>").text((data.main.temp / 10).toFixed(2) + "°C (feels like " + (data.main.feels_like / 10).toFixed(2) + " °C)"))
+                        .append($("<p></p>").text("Humidity: " + data.main.humidity + "%"))
+                        .append($("<p></p>").text("Wind speed: " + data.wind.speed + " m/s"))
+                    )
+                );
+
+            console.log("weather data", data)
+        });
+
+        // let mapUrl = "https://api.mapbox.com/directions/v5/mapbox/driving-traffic/151.2073, -33.8679";
+        // $.get(mapUrl, function (data) {
+        //     console.log("map data", data)
+        // })
+    });
 }
 
 function getMapAPI() {
