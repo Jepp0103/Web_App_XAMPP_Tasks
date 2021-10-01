6ab7b7c2705e788a9c837427d6e41a9e7073e5fc
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-10-01 21:52:48 +0200

    Actors can now be displayed on the page from the API

--- mandatory_1_movie_db/js/movie_db.js
+++ mandatory_1_movie_db/js/movie_db.js
@@ -1,15 +1,41 @@
+let apiKey = "40876da1f83f0c135767cb0b759c57f3";
+let searchTypeClickCount = 0;
 
 $(document).ready(function () {
-    $("#movieBtn").click(function () {
-        getMovieDbApi();
+    $("#personSearchType").click(function () { //Button to choose type of search - movies or people
+        if (searchTypeClickCount % 2 === 0) {
+            $("#personSearchType").text("Filter by movies")
+            $("#movieInput").attr("placeholder", "Type name of personality")
+            $("#movieBtn").remove();
+            $("#movieSearch").append($("<button></button>").attr("id", "peopleBtn").text("Search for movie personalities"));
+            clickSearchButtons();
+        } else {
+            $("#personSearchType").text("Filter by personalities")
+            $("#movieInput").attr("placeholder", "Type movie or production year")
+            $("#peopleBtn").remove();
+            $("#movieSearch").append($("<button></button>").attr("id", "movieBtn").text("Search for movies"));
+            clickSearchButtons();
+        }
+        searchTypeClickCount++;
     })
+    clickSearchButtons();
 });
 
-function getMovieDbApi() {
-    let inputMovieTitle = $("#movieInput").val();
+function clickSearchButtons() {
+    //Buttons for getting apis for people or movies
+    $("#peopleBtn").click(function () {
+        getPersonsApi();
+    })
+
+    $("#movieBtn").click(function () {
+        getMoviesApi();
+    })
+}
+
+function getMoviesApi() {
+    let inputMovieTitle = $("#searchInput").val();
     console.log("input movie title", inputMovieTitle)
     let baseUrl = "https://api.themoviedb.org/3/search/movie/?api_key=";
-    let apiKey = "40876da1f83f0c135767cb0b759c57f3";
     let titleParam = "&language=en-US&query=" + inputMovieTitle;
     let regNumExp = /\d{4}/;
 
@@ -29,14 +55,38 @@ function getMovieDbApi() {
     }
 }
 
+function getPersonsApi() {
+    let inputPersonName = $("#searchInput").val();
+    let baseUrl = "https://api.themoviedb.org/3/search/person/?api_key=";
+    let personNameParam = "&language=en-US&query=" + inputPersonName;
+
+    $.post(baseUrl + apiKey + personNameParam, function (data) {
+        displayPersonsData(data);
+    });
+}
+
+function displayPersonsData(data) {
+    console.log("persons display data", data)
+    $("#displayedData").remove(); //Removes already displayed data before appending new ones
+    $("main").append($("<section><section>").attr("id", "displayedData"));
+
+    for (let i = 0; i < data.results.length; i++) { //Iterating through the persons data list.
+        $("#displayedData")
+            .append($("<div></div>")
+                .append("<p>" + data.results[i].name + "</p>")
+                .append($("<img>").attr("src", "https://image.tmdb.org/t/p/original/" + data.results[i].profile_path).attr("class", "personImages"))
+                .append("<p>" + data.results[i].known_for_department + "</p>")
+            )
+    }
+}
+
 function displayMovieData(data) {
     console.log("display data", data)
-    $("#displayedMovies").remove(); //Removes already displayed movies before appending new ones.
-    $("main").append($("<section><section>").attr("id", "displayedMovies"));
+    $("#displayedData").remove(); //Removes already displayed data before appending new ones.
+    $("main").append($("<section><section>").attr("id", "displayedData"));
 
-    for (let i = 0; i < data.results.length; i++) { //Iterating through the movie list.
-        // console.log("title:", data.results[i].title, "release year:", data.results[i].release_date, "language:", data.results[i].original_language);
-        $("#displayedMovies")
+    for (let i = 0; i < data.results.length; i++) { //Iterating through the movies data list.
+        $("#displayedData")
             .append($("<div></div>")
                 .append("<p>" + data.results[i].title + "</p>")
                 .append($("<img>").attr("src", "https://image.tmdb.org/t/p/original/" + data.results[i].poster_path).attr("class", "movieImages"))
@@ -45,3 +95,4 @@ function displayMovieData(data) {
             )
     }
 }
+
