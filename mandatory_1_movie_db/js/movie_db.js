d4a040c016ce351b2415059512b9b1445c3f1d79
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-10-01 23:49:25 +0200

    Working on displaying detaile dialog about each movie and person searched for from the api

--- mandatory_1_movie_db/js/movie_db.js
+++ mandatory_1_movie_db/js/movie_db.js
@@ -2,18 +2,19 @@ let apiKey = "40876da1f83f0c135767cb0b759c57f3";
 let searchTypeClickCount = 0;
 
 $(document).ready(function () {
-    $("#personSearchType").click(function () { //Button to choose type of search - movies or people
+    $("#searchTypeBtn").click(function () { //Button to choose type of search - movies or people
+        console.log("search type clicking", searchTypeClickCount)
         if (searchTypeClickCount % 2 === 0) {
-            $("#personSearchType").text("Filter by movies")
-            $("#movieInput").attr("placeholder", "Type name of personality")
+            $("#searchTypeBtn").text("Filter by movies")
+            $("#searchInput").attr("placeholder", "Type name of personality")
             $("#movieBtn").remove();
-            $("#movieSearch").append($("<button></button>").attr("id", "peopleBtn").text("Search for movie personalities"));
+            $("#searchSection").append($("<button></button>").attr("id", "peopleBtn").text("Search for movie personalities"));
             clickSearchButtons();
         } else {
-            $("#personSearchType").text("Filter by personalities")
-            $("#movieInput").attr("placeholder", "Type movie or production year")
+            $("#searchTypeBtn").text("Filter by personalities")
+            $("#searchInput").attr("placeholder", "Type movie or production year")
             $("#peopleBtn").remove();
-            $("#movieSearch").append($("<button></button>").attr("id", "movieBtn").text("Search for movies"));
+            $("#searchSection").append($("<button></button>").attr("id", "movieBtn").text("Search for movies"));
             clickSearchButtons();
         }
         searchTypeClickCount++;
@@ -34,23 +35,19 @@ function clickSearchButtons() {
 
 function getMoviesApi() {
     let inputMovieTitle = $("#searchInput").val();
-    console.log("input movie title", inputMovieTitle)
     let baseUrl = "https://api.themoviedb.org/3/search/movie/?api_key=";
     let titleParam = "&language=en-US&query=" + inputMovieTitle;
     let regNumExp = /\d{4}/;
 
-    if (regNumExp.test(inputMovieTitle)) {
+    if (regNumExp.test(inputMovieTitle)) { //Validating if input has year number in it
         let inputMovieYear = inputMovieTitle.match(regNumExp)[0]; //Getting the first 4 digits in search input with regex
         let yearParam = "&page=1&include_adult=false&year=" + inputMovieYear;
-        console.log("input movie year", inputMovieYear)
         $.post(baseUrl + apiKey + titleParam + yearParam, function (data) {
-            console.log("movie api data with year")
             displayMovieData(data)
         });
     } else {
         $.post(baseUrl + apiKey + titleParam, function (data) {
-            console.log("movie api data without year")
-            displayMovieData(data)
+            displayMoviesData(data)
         });
     }
 }
@@ -67,12 +64,11 @@ function getPersonsApi() {
 
 function displayPersonsData(data) {
     console.log("persons display data", data)
-    $("#displayedData").remove(); //Removes already displayed data before appending new ones
-    $("main").append($("<section><section>").attr("id", "displayedData"));
+    $("#displayedData").empty(); //Removes already displayed data before appending new ones
 
     for (let i = 0; i < data.results.length; i++) { //Iterating through the persons data list.
         $("#displayedData")
-            .append($("<div></div>")
+            .append($("<div></div>").attr("class", "dataDivs")
                 .append("<p>" + data.results[i].name + "</p>")
                 .append($("<img>").attr("src", "https://image.tmdb.org/t/p/original/" + data.results[i].profile_path).attr("class", "personImages"))
                 .append("<p>" + data.results[i].known_for_department + "</p>")
@@ -80,19 +76,33 @@ function displayPersonsData(data) {
     }
 }
 
-function displayMovieData(data) {
-    console.log("display data", data)
-    $("#displayedData").remove(); //Removes already displayed data before appending new ones.
-    $("main").append($("<section><section>").attr("id", "displayedData"));
+function displayMoviesData(data) {
+    $("#displayedData").empty(); //Removes already displayed data before appending new ones.
 
     for (let i = 0; i < data.results.length; i++) { //Iterating through the movies data list.
         $("#displayedData")
-            .append($("<div></div>")
+            .append($("<div></div>").attr("class", "dataDivs")
                 .append("<p>" + data.results[i].title + "</p>")
                 .append($("<img>").attr("src", "https://image.tmdb.org/t/p/original/" + data.results[i].poster_path).attr("class", "movieImages"))
                 .append("<p>" + data.results[i].release_date + "</p>")
                 .append("<p>" + data.results[i].original_language + "</p>")
             )
     }
+
+    $(".dataDivs").click(function () {
+        displayMovieDetails($(this), data)
+    })
+}
+
+function displayPersonDetails() {
+}
+
+function displayMovieDetails(movieDataDiv, data) {
+    console.log("displaying movie details and elm", movieDataDiv)
+    console.log("movie data details", data)
+    $('#detailsDialog').dialog({
+        autoOpen: false
+    });
+    $("#detailsDialog").dialog('open');
 }
 
