327c67188f9d1f66225a0e6c91e5aa3c088f818e
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-10-04 17:35:36 +0200

    Can now display detailed movie data in a dialog, when clicking on a specific movie

--- mandatory_1_movie_db/js/movie_db.js
+++ mandatory_1_movie_db/js/movie_db.js
@@ -3,7 +3,6 @@ let searchTypeClickCount = 0;
 
 $(document).ready(function () {
     $("#searchTypeBtn").click(function () { //Button to choose type of search - movies or people
-        console.log("search type clicking", searchTypeClickCount)
         if (searchTypeClickCount % 2 === 0) {
             $("#searchTypeBtn").text("Filter by movies")
             $("#searchInput").attr("placeholder", "Type name of personality")
@@ -68,7 +67,7 @@ function displayPersonsData(data) {
 
     for (let i = 0; i < data.results.length; i++) { //Iterating through the persons data list.
         $("#displayedData")
-            .append($("<div></div>").attr("class", "dataDivs")
+            .append($("<div></div>").attr("class", "dataDivs").attr("id", "person" + i)
                 .append("<p>" + data.results[i].name + "</p>")
                 .append($("<img>").attr("src", "https://image.tmdb.org/t/p/original/" + data.results[i].profile_path).attr("class", "personImages"))
                 .append("<p>" + data.results[i].known_for_department + "</p>")
@@ -81,28 +80,55 @@ function displayMoviesData(data) {
 
     for (let i = 0; i < data.results.length; i++) { //Iterating through the movies data list.
         $("#displayedData")
-            .append($("<div></div>").attr("class", "dataDivs")
+            .append($("<div></div>").attr("class", "dataDivs").attr("id", "movie" + i)
                 .append("<p>" + data.results[i].title + "</p>")
                 .append($("<img>").attr("src", "https://image.tmdb.org/t/p/original/" + data.results[i].poster_path).attr("class", "movieImages"))
                 .append("<p>" + data.results[i].release_date + "</p>")
                 .append("<p>" + data.results[i].original_language + "</p>")
-            )
+            );
     }
 
     $(".dataDivs").click(function () {
-        displayMovieDetails($(this), data)
+        displayMovieDetails($(this).attr("id"), data);
     })
 }
 
 function displayPersonDetails() {
 }
 
-function displayMovieDetails(movieDataDiv, data) {
-    console.log("displaying movie details and elm", movieDataDiv)
-    console.log("movie data details", data)
+function displayMovieDetails(movieDivId, data) {
+    movieIdNumber = parseInt(movieDivId.substring(5, 6));
+    console.log("movie data details", data.results[movieIdNumber]);
     $('#detailsDialog').dialog({
         autoOpen: false
     });
+
+    //Defining another movie endpoint from the movie api with more movie info
+    let movieApiId = data.results[movieIdNumber].id;
+    let detailedMovieEndpoint = "https://api.themoviedb.org/3/movie/" + movieApiId + "?api_key=" + apiKey;
+
+    $.get(detailedMovieEndpoint, function (detailedData) {
+        console.log("Other movie data", (detailedData));
+
+        $("#detailsDialog")
+            .empty()
+            .append("<h3>" + data.results[movieIdNumber].title + "</h3>")
+            .append($("<img>").attr("src", "https://image.tmdb.org/t/p/original/" + data.results[movieIdNumber].poster_path).attr("class", "movieImages"))
+            .append($("<p></p>").text("Release date:").attr("class", "movieAttributes"))
+            .append("<p>" + data.results[movieIdNumber].release_date + "</p>")
+            .append($("<p></p>").text("Original language:").attr("class", "movieAttributes"))
+            .append("<p>" + data.results[movieIdNumber].original_language + "</p>")
+            .append($("<p></p>").text("Runtime:").attr("class", "movieAttributes"))
+            .append("<p>" + detailedData.runtime + " minutes</p>")
+            .append($("<p></p>").text("Overview:").attr("class", "movieAttributes"))
+            .append("<p>" + data.results[movieIdNumber].overview + "</p>")
+            .append($("<a></a>").attr("href", detailedData.homepage).attr("class", "movieAttributes").text("Visit web page here"))
+            .append($("<p></p>").text("Genres:").attr("class", "movieAttributes"))
+
+        for (let i = 0; i < detailedData.genres.length; i++) { //Appending genres
+            $("#detailsDialog").append("<li>" + detailedData.genres[i].name + "</li>")
+        }
+    });
+
     $("#detailsDialog").dialog('open');
 }
-
