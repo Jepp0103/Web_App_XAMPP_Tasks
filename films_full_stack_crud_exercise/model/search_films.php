45d80dfea649fc312f16773c525bea2c2eb6e4a9
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-11-07 13:35:44 +0100

    Starting on full stack application php

--- films_full_stack_crud_exercise/model/search_films.php
+++ films_full_stack_crud_exercise/model/search_films.php
@@ -1,25 +1,29 @@
 <?php
     include "films_db.php";
 
-    $films_query = <<<"SQL"
-                        SELECT title, release_date, runtime
-                        FROM movie LIMIT 20
-                        SQL;
+    getSearchedMovies();
+
+    echo "helllo";
 
 
-    $films_exec = $pdo->query($films_query);
+    function getSearchedMovies() {
+        // if(isset($_POST["filmTitleInput"]) && $_POST["filmTitleInput"] != null && $_POST["filmTitleInput"]!= "") {
+            $film_input_title = $_POST["filmTitleInput"];
 
-    $films_data = $films_exec->fetchAll();
+            $films_query = <<<"SQL"
+                            SELECT title, release_date, runtime
+                            FROM movie WHERE title = $film_input_title
+                            SQL;
 
+            $films_exec = $pdo->query($films_query);
+            $films_data = $films_exec->fetchAll();
+            echo json_encode($films_data);
+        // }
+    }
+        // $departments_query .= " ORDER BY de.dept_name;";
+        // $departments = $pdo->query($departments_query);
 
-    // $departments_query .= " ORDER BY de.dept_name;";
-    // $departments = $pdo->query($departments_query);
-
-    // $departments_query .= " ORDER BY manager;";
-    // $departments = $pdo->query($departments_query);
-    // $departments = $pdo->query($departments_query);
-    
-    echo json_encode($films_data);
-
-  
+        // $departments_query .= " ORDER BY manager;";
+        // $departments = $pdo->query($departments_query);
+        // $departments = $pdo->query($departments_query);
 ?>
--- films_full_stack_crud_exercise/view/js/films.js
+++ films_full_stack_crud_exercise/view/js/films.js
@@ -1,15 +1,26 @@
-getFilms()
+searchFilms()
 
-function getFilms() {
-    const filmsEndpoint = "http://localhost/web_app_xampp_tasks/Films_full_stack_crud_exercise/model/search_films.php";
-    $.ajax({
-        url: filmsEndpoint,
-        type: "GET",
-        dataType: "json",
-        processData: false,
-        contentType: false
-    }).done(function (data) {
-        displayMovies(data)
+function searchFilms() {
+    $("#searchMovieBtn").click(function () {
+        const filmTitleInput = $("#filmTitleInput").val()
+        console.log("filmtitleinput", filmTitleInput)
+        if (filmTitleInput != null || filmTitleInput != "") {
+            console.log("getting here??")
+            const filmsEndpoint = "http://localhost/web_app_xampp_tasks/Films_full_stack_crud_exercise/model/search_films.php";
+            let form = new FormData();
+            form.append("filmTitleInput", filmTitleInput);
+            $.ajax({
+                url: filmsEndpoint,
+                type: "POST",
+                data: form,
+                dataType: "json",
+                processData: false,
+                contentType: false
+            }).done(function (data) {
+                console.log("Data:", data)
+                displayMovies(data)
+            })
+        }
     })
 }
 
