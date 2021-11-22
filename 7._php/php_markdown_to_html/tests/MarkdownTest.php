b9484024de681a1577af88142c21a52f29701b6b
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-11-22 19:21:14 +0100

    Learning php unit testing with composer

--- 7._php/php_markdown_to_html/tests/MarkdownTest.php
+++ 7._php/php_markdown_to_html/tests/MarkdownTest.php
@@ -6,14 +6,40 @@
     use \PHPUnit\Framework\TestCase;
 
     class MarkdownToHtmlTest extends TestCase {
-        
-        public function testIsOl() { //Write test keyword in front of the method name
+
+        public function testIsOlTrue() { //Write test keyword in front of the method name
             $text = '6. Hello there'; // Arrange
             $result = isOL($text); // Act
-            $this->assertEquals(true, $result, "Expected $text");   // Assert
+            $this->assertEquals(true, $result, "Actual: $text");   // Assert
         }
 
+        public function testIsOlFalse(){
+            $text = 'Hello there'; // Arrange
+            $result = isOL($text); // Act
+            $this->assertEquals(false, $result, "Actual: $text");   // Assert
+        }
 
+        public function testIsOlFails(){
+            $text = 'Hello there'; // Arrange
+            $result = isOL($text); // Act
+            $this->assertEquals(true, $result, "Actual: $text");   // Assert
+        }
+
+        public function testReplaceByHtmlTagsStrong() {
+            $text = "I just love **bold text**."; //Arrange
+            $result = replaceByHTMLTags($text, '**', 'strong');
+            $this->assertEquals("I just love <strong>bold text</strong>.", $result, "Actual: $result");
+        }
+
+        public function testReplaceByHtmlTagsEm() {
+            $text = "I just love *em text*.";
+            $result = replaceByHTMLTags($text, '*', 'em');
+            $this->assertEquals("I just love <em>em text</em>.", $result, "Actual: $result");
+        }
+
+        public function testReplaceImages() {
+            $text = "![Tux, the Linux mascot](/assets/images/tux.png)";
+        }
     }
 
 ?>
