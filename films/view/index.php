fc8ba0579edf813f20d2995bfdc2593d79996c85
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-11-24 20:56:09 +0100

    Working on authentication exercise for the films crud application

--- films/view/index.php
+++ films/view/index.php
@@ -1,7 +1,10 @@
 <?php 
     include('header.html');
     
-    include('films_page.html');
+    // include('films_page.html');
+
+    include('login_page.html');
+
     
     include('footer.html'); 
 ?>
